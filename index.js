f9a5b0b65958ad06422938bb24e9b5e2466db89b
Nasko Trenchev <naskotrenchev1@gmail.com>
2023-04-27 11:26:45 +0300

    d

--- index.js
+++ index.js
@@ -1,10 +1,1 @@
 console.log(Yes);
-console.log("No");
-console.log("Nope");
-console.log("Nopde");
-console.log("Nopde");
-console.log("Nopde");
-console.log("Nopde");
-console.log("Last");
-console.log("ba");
-console.log("ba");
